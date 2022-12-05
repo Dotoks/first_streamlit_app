e2f9c3789a4313210bfc50867e29f98139ab711a
Ivo <53398611+Dotoks@users.noreply.github.com>
2022-12-05 10:03:42 +0200

    Update streamlit_app.py
    Added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Oatmeals with protein')
+streamlit.text('Hard-boiled eggs')
+streamlit.text('Sandwiches with ham')
